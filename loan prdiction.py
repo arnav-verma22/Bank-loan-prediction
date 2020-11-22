79c80983eaf731bd5e0ede7e74479b05f7478808
arnav-verma22 <arn.verma22@gmail.com>
2020-11-22 09:29:13 +0530

    functions of missing values

--- loan prdiction.py
+++ loan prdiction.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import numpy as np
+from multipledispatch import dispatch
 df = pd.read_csv("train.csv")
 
 from sklearn.impute import SimpleImputer
-missingvalues = SimpleImputer(missing_values =np.nan, strategy="constant",fill_value='0', verbose = 0)
-missingvalues = missingvalues.fit([df['Dependents']])
-[df['Dependents']]=missingvalues.transform([df['Dependents']])
 
-missingvalues = SimpleImputer(missing_values ='3+', strategy="constant",fill_value='4', verbose = 0)
-missingvalues = missingvalues.fit([df['Dependents']])
-[df['Dependents']]=missingvalues.transform([df['Dependents']])
+def replace_missing(data, *args):
+    if len(args) == 3:
+        missingvalues = SimpleImputer(missing_values=args[0], strategy=args[1], fill_value=args[2], verbose=0)
+        missingvalues = missingvalues.fit([data])
+        [data] = missingvalues.transform([data])
+
+    if len(args) == 2:
+        missingvalues = SimpleImputer(missing_values=args[0], strategy=args[3], verbose=0)
+        missingvalues = missingvalues.fit([data])
+        [data] = missingvalues.transform([data])
+
+    return data
+
+df['Dependents'] = replace_missing(df['Dependents'], np.nan,'constant', '0')
+
+df['Dependents']=replace_missing(df['Dependents'], '3+', 'constant', '4')
 
 df['Dependents'] = df['Dependents'].astype(int)

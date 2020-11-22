011baca36f4d732127c987fd9108b79fc967e80f
arnav-verma22 <arn.verma22@gmail.com>
2020-11-22 15:24:28 +0530

    missing values of all columns removed

--- loan prdiction.py
+++ loan prdiction.py
@@ -12,7 +12,7 @@ def replace_missing(data, *args):
         [data] = missingvalues.transform([data])
 
     if len(args) == 2:
-        missingvalues = SimpleImputer(missing_values=args[0], strategy=args[3], verbose=0)
+        missingvalues = SimpleImputer(missing_values=args[0], strategy=args[1], verbose=0)
         missingvalues = missingvalues.fit([data])
         [data] = missingvalues.transform([data])
 
@@ -23,3 +23,18 @@ df['Dependents'] = replace_missing(df['Dependents'], np.nan,'constant', '0')
 df['Dependents']=replace_missing(df['Dependents'], '3+', 'constant', '4')
 
 df['Dependents'] = df['Dependents'].astype(int)
+
+df.reset_index()
+
+'''df['Gender']=replace_missing(df['Gender'], np.nan, 'most_frequent')
+
+missingvalues = SimpleImputer(missing_values=np.nan, strategy='median')
+missingvalues = missingvalues.fit([df['Loan_Amount_Term']])
+[df['Loan_Amount_Term']] = missingvalues.transform([df['Loan_Amount_Term']])
+
+
+frequent = ['Gender', 'Married', 'Loan_Amount_Term', 'Self_Employed', 'Credit_History', 'Loan_Amount']'''
+
+for i in df.columns:
+    if df[i].isnull().values.any() == True:
+        df[i]=replace_missing(df[i], np.nan, 'constant',df[i].mode()[0])
